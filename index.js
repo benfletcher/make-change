c7c5789c281fdcbb0e85853bcd222df365022036
bfletcher <bfletch@gmail.com>
2018-12-20 18:41:32 -0500

    essential functionality complete.

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-// var program = require("commander");
+/*
+Given an amount in Bills that can be (1,2,5,10,20,50,100) change it to coins that are (0.01,0.05,0.10,0.25). The machine needs to assume there is a finite number of coins.
+ 
+Assumptions:
+       
+1.        Start with a 100 coins of each.
+2.        machine should return the least amount of coins possible;
+3.        Machine should display a message if it do not have have enough coins;
+4.        Code should maintain the state of coins (coins left) throughout all the transaction till it runs out of the coin then it should exit.
+5.        Deliver the code with test cases.
 
-console.log(process.argv);
-
-// program
-//   .version("0.1.0")
-//   .usage("[options] <ordered list of bills ...>")
-//   .option("-b, --bills <n>", "Comma separated list of bills", billsList =>
-//     billsList.split(",").map(Number)
-//   )
-//   .parse(process.argv);
-
-// console.log(program.bills);
-
-// while (program.change.length) {
-//   const currBill = program.change;
-// }
+*/
 
 const STARTING_COUNT = 100;
 const COIN_VALUES = [25, 10, 5, 1];
 
-class makeChange {
+class MakeChange {
   constructor({
     bills = [],
     startingCoinsQty = STARTING_COUNT,
     coinDenominations = COIN_VALUES
   } = {}) {
     this.bills = bills;
+
     this.coinCounts = new Map(
-      coinDenominations.map(value => [value, startingCoinsQty])
+      coinDenominations
+        .sort((a, b) => b - a) // code relies on decreasing values in order
+        .map(value => [value, startingCoinsQty])
     );
   }
 
@@ -42,13 +40,46 @@ class makeChange {
     return this.totalCoinValue() >= bill;
   }
 
-  processOneBillFromList() {
-    const bill = this.bills.shift();
+  processOneBill(billToChange) {
+    let bill = billToChange * 100; // Dealing with $$ and base 2 Number type -> use pennies
 
-    const coins = new Map([[25, 4]]);
+    if (!this.canMakeChange(bill / 100)) {
+      return {
+        success: false,
+        message:
+          `Sorry only $${this.totalCoinValue()} in coins remaining.\n` +
+          `Can't make change for a $${bill / 100} bill.`
+      };
+    }
+
+    const coins = { success: true, message: "Success" };
+
+    while (bill > 0 && this.coinCounts.size > 0) {
+      const [biggestDenom, qty] = [...this.coinCounts][0];
+
+      if (qty === 0) {
+        this.coinCounts.delete(biggestDenom);
+        continue;
+      }
+
+      bill -= biggestDenom;
+      coins[biggestDenom] = (coins[biggestDenom] || 0) + 1;
+      this.coinCounts.set(biggestDenom, qty - 1);
+    }
 
     return coins;
   }
+
+  processBills() {
+    const output = [];
+    while (this.bills.length > 0) {
+      let bill = this.bills.shift();
+
+      output.push([bill, this.processOneBill(bill)]);
+    }
+
+    return output;
+  }
 }
 
-module.exports = makeChange;
+module.exports = MakeChange;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,32 +1,121 @@
-const makeChange = require("../index");
+const MakeChange = require("../index");
 
 describe("Make change for specified list of bills", () => {
   it("accepts a list of bills", () => {
     const bills = [5, 10];
-    const changer = new makeChange({ bills });
+    const changer = new MakeChange({ bills });
     expect(changer.bills).toEqual([5, 10]);
   });
 
   it("properly calculates total coins value", () => {
-    const changer = new makeChange();
+    const changer = new MakeChange();
     expect(changer.totalCoinValue()).toBe(41);
   });
 
   it("can determine whether change is possible for a tenner", () => {
-    const changer = new makeChange();
+    const changer = new MakeChange();
     expect(changer.canMakeChange(10)).toBeTruthy();
   });
 
   it("can determine whether change is possible for a benjamin", () => {
-    const changer = new makeChange();
+    const changer = new MakeChange();
     expect(changer.canMakeChange(100)).toBeFalsy();
   });
 
-  it("can process one bill from list", () => {
-    const bills = [1, 5, 10];
-    const changer = new makeChange({ bills });
-    const resultCoins = changer.processOneBillFromList();
+  it("throws if can't make change", () => {
+    const bills = [100, 50];
+    const changer = new MakeChange({ bills });
+    const output = changer.processOneBill(100);
 
-    expect(resultCoins.get(25)).toEqual(4);
+    expect(output.success).toBeFalsy;
+  });
+
+  describe("can process one bill from list, 1 dollar bill", () => {
+    const bills = [1, 5, 10];
+    const changer = new MakeChange({ bills });
+    const resultCoins = changer.processOneBill(1);
+
+    it("calculates coins for one dollar", () => {
+      expect(resultCoins[25]).toEqual(4);
+    });
+  });
+
+  describe("can process one bill from list, 5 dollar bill", () => {
+    const bills = [5, 10];
+    const changer = new MakeChange({ bills });
+    const resultCoins = changer.processOneBill(5);
+
+    it("calculates coins for 5 dollar bill", () => {
+      expect(resultCoins[25]).toEqual(20);
+    });
+  });
+
+  it("can process list of coins without throwing", () => {
+    const bills = [20, 5, 10];
+    const changer = new MakeChange({ bills });
+
+    expect(changer.processBills.bind(changer)).not.toThrow();
+  });
+
+  it("can process list of 1 coin", () => {
+    const bills = [20];
+    const changer = new MakeChange({ bills });
+    const output = changer.processBills();
+    const expected = [[20, { success: true, message: "Success", 25: 80 }]];
+
+    expect(output).toEqual(expected);
+  });
+
+  it("can process list of 2 coins", () => {
+    const bills = [20, 5];
+    const changer = new MakeChange({ bills });
+    const output = changer.processBills();
+    const expected = [
+      [20, { success: true, message: "Success", 25: 80 }],
+      [5, { success: true, message: "Success", 25: 20 }]
+    ];
+
+    expect(output).toEqual(expected);
+  });
+
+  it("can process list of coins and use all coins", () => {
+    const bills = [20, 20, 1];
+    const changer = new MakeChange({ bills });
+    const output = changer.processBills();
+
+    const expected = [
+      [20, { success: true, message: "Success", "25": 80 }],
+      [
+        20,
+        { success: true, message: "Success", "5": 100, "10": 100, "25": 20 }
+      ],
+      [1, { success: true, message: "Success", "1": 100 }]
+    ];
+
+    expect(output).toEqual(expected);
+  });
+
+  it("can process list of coins and run out of coins", () => {
+    const bills = [20, 20, 5];
+    const changer = new MakeChange({ bills });
+    const output = changer.processBills();
+
+    const expected = [
+      [20, { success: true, message: "Success", "25": 80 }],
+      [
+        20,
+        { success: true, message: "Success", "5": 100, "10": 100, "25": 20 }
+      ],
+      [
+        5,
+        {
+          success: false,
+          message:
+            "Sorry only $1 in coins remaining.\nCan't make change for a $5 bill."
+        }
+      ]
+    ];
+
+    expect(output).toEqual(expected);
   });
 });
